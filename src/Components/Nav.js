43225436e9f0ed61db55bcd146b190d13ec4ae10
Angel <EMAIL>
2023-07-22 15:23:37 -0400

    changes to nav and home

---
+++ src/Components/Nav.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+// import "../Css/Nav.css";
+
+function Nav() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          {/* <Link</Link> */}
+        </li>
+
+        <li>
+          {/* <Link</Link> */}
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,3 +1,17 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+// import "../Css/Home.css"
+
 export default function Home() {
+
+    return (
+        <>
+        <div className='container'>
+        <Link to ={`//`}>
+            <h1 className='header'>Title of home page</h1>
+        </Link>
+        </div>
+        </>
+    )
     
 }
