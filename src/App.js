cb07e80ad6344de0deebf30308807c449830207e
MaxWattan <mcwattanac@gmail.com>
2023-07-22 12:31:46 -0400

    added routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Nav from "./Components/Nav"
+
+import Home from "./Pages/Home"
+import Location from "./Pages/Location"
+import Info from "./Pages/Info"
+import Petition from "./Petition"
+import Error from "./Pages/Error"
+import Team from "./Pages/Team"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+       <Nav />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Location/>} />
+          <Route path="/" element={<Info />} />
+          <Route path="/" element={<Petition />} />
+          <Route path="/" element={<Error />} />
+          <Route path="/" element={<Team />} />
+
+        </Routes>
+
+      </main>
+     </Router>
     </div>
   );
 }
