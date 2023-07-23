449074a367cd6918d0f46c624f6a2f10e667419e
JohnnyMCR <jmcastillo991@gmail.com>
2023-07-23 13:51:23 -0400

    new changes for info.css and cleanup code

--- src/Components/Petitionform.js
+++ src/Components/Petitionform.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {useState} from 'react'
+import { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 
 const API = process.env.REACT_APP_API_URL
@@ -9,14 +9,14 @@ export default function Petition() {
 
     const addPetition = (newPetition) => {
         axios
-        .post(`${API}/index/petitionform`, newPetition)
-        .then(
-            () => {
-                navigate(`/index/petitionform`)
-            }, 
-            (error) => console.error(error)
-        )
-        .catch((c) => console.warn("catch", c));
+            .post(`${API}/index/petitionform`, newPetition)
+            .then(
+                () => {
+                    navigate(`/index/petitionform`)
+                },
+                (error) => console.error(error)
+            )
+            .catch((c) => console.warn("catch", c));
     }
     const [petition, setPetition] = useState({
         name: "",
@@ -27,64 +27,63 @@ export default function Petition() {
     })
 
     const handleTextChange = (event) => {
-        setPetition({...petition, [event.tatget.id]: event.target.value})
+        setPetition({ ...petition, [event.tatget.id]: event.target.value })
     }
     const handleSubmit = (event) => {
         event.preventDefault()
         addPetition(petition)
     }
-  return (
-    <div className='petitionform'>
-    <Link to={`/index/petitionform/petitioners`}>
-    <button>List of Petitioners</button>
-    </Link>
+    return (
+        <div className='petitionform'>
+            <Link to={`/index/petitionform/petitioners`}>
+                <button>List of Petitioners</button>
+            </Link>
 
-<form onSubmit={handleSubmit}>
-    <label htmlFor="name"> Name: </label>
-    <input 
-    id="name"
-    type="text"
-    placeholder='First and Last Name of Petitioner'
-    onChange={handleTextChange}
-    required/>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name"> Name: </label>
+                <input
+                    id="name"
+                    type="text"
+                    placeholder='First and Last Name of Petitioner'
+                    onChange={handleTextChange}
+                    required />
 
-<label htmlFor="borough"> Borough: </label>
-    <input 
-    id="borough"
-    type="text"
-    placeholder='Borough Location'
-    onChange={handleTextChange}
-    required/>
+                <label htmlFor="borough"> Borough: </label>
+                <input
+                    id="borough"
+                    type="text"
+                    placeholder='Borough Location'
+                    onChange={handleTextChange}
+                    required />
 
-<label htmlFor="address"> Address: </label>
-    <input 
-    id="address"
-    type="text"
-    placeholder='Address of Petitioner'
-    onChange={handleTextChange}
-    required/>
+                <label htmlFor="address"> Address: </label>
+                <input
+                    id="address"
+                    type="text"
+                    placeholder='Address of Petitioner'
+                    onChange={handleTextChange}
+                    required />
 
-<label htmlFor="bin_mpg"> Bin(Metal, Plastic, Glass): </label>
-    <input 
-    id="bin_mpg"
-    type="text"
-    placeholder='Amount Of Bin'
-    onChange={handleTextChange}
-    required/>
+                <label htmlFor="bin_mpg"> Bin(Metal, Plastic, Glass): </label>
+                <input
+                    id="bin_mpg"
+                    type="text"
+                    placeholder='Amount Of Bin'
+                    onChange={handleTextChange}
+                    required />
 
-<label htmlFor="bin_p"> Bin(Paper): </label>
-    <input 
-    id="bin_p"
-    type="text"
-    placeholder='Amount Of Bin'
-    onChange={handleTextChange}
-    required/>
-    
-    <br></br>
-    <input type="submit" />
+                <label htmlFor="bin_p"> Bin(Paper): </label>
+                <input
+                    id="bin_p"
+                    type="text"
+                    placeholder='Amount Of Bin'
+                    onChange={handleTextChange}
+                    required />
 
-</form>
+                <br></br>
+                <input type="submit" />
 
-    </div>
-  )
+            </form>
+        </div>
+    )
 }
--- src/Pages/Petitionform.js
+++ src/Pages/Petitionform.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import Petitionform from '../Components/Petitionform'
 import "../Css/Petitionform.css"
-
 
 export default function Petition() {
   return (
     <div>
-        <Petitionform/>
-       
+      <Petitionform />
     </div>
   )
 }
