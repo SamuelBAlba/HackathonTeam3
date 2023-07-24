f00450fcfcaac49daf07e97a489bec437fec7de1
Angel <EMAIL>
2023-07-24 01:53:00 -0400

    deleted petition

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Link } from "react-router-dom"
 import Locations from '../Components/Locations'
 import "../Css/IndexLocation.css"
 
 export default function Index() {
   return (
     <div className='Index'>
-      <button>
-        <Link to="/index/petitionform">Request A Location</Link>
-      </button>
       <Locations />
     </div>
   )
